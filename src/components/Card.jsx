02b32b7c474afcaab52ec92891dd378ca9d7a60d
LF-creator <linar.fatkullin@yh.nackademin.se>
2025-06-09 16:26:51 +0200

    Cardjsx update to fix style

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,13 +3,15 @@ import '../styles/Card.css';
 
 const Card = ({ label, value, icon: Icon }) => {
   return (
-<div className="card-header">
-  <div className="icon-bubble">
-    {Icon && <Icon className="card-icon" />}
-  </div>
-  <p className="card-label">{label}</p>
-</div>
+    <div className="card">
+      <div className="card-header">
+        <div className="icon-bubble">
+          <Icon className="card-icon" />
+        </div>
+        <p className="card-label">{label}</p>
+      </div>
+      <p className="card-value">{value}</p>
+    </div>
   );
 };
-
 export default Card;
