9220704cb747eb7d7be7d3de4e4f16d38a24324d
LF-creator <linar.fatkullin@yh.nackademin.se>
2025-06-10 12:43:42 +0200

    Added label to pie chart

--- src/components/GenderSection.jsx
+++ src/components/GenderSection.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { PieChart, Pie, Cell, ResponsiveContainer } from "recharts";
+import { PieChart, Pie, Cell, ResponsiveContainer,Label } from "recharts";
 import "../styles/GenderSection.css";
 
 const GenderSection = ({ gender }) => {
@@ -7,6 +7,19 @@ const GenderSection = ({ gender }) => {
     { name: "Female", value: gender.female },
     { name: "Male", value: gender.male },
   ];
+
+    const percentage = (
+    (data[0].value / (data[0].value + data[1].value)) *
+    100
+  ).toFixed(0);
+  const startAngle = 180; 
+  const endAngle = 0; 
+  const totalValue = data.reduce((acc, item) => acc + item.value, 0);
+  const firstSegmentValue = data[0].value;
+  const firstSegmentPercentage = firstSegmentValue / totalValue;
+  const firstSegmentEndAngle = startAngle - (startAngle - endAngle) * firstSegmentPercentage;
+  const midpointAngle = firstSegmentEndAngle - (firstSegmentEndAngle - endAngle) / 2;
+  const radius = 70;
 
   const COLORS = ["#00d9c0", "#4cc6f3"];
 
@@ -31,8 +44,13 @@ const GenderSection = ({ gender }) => {
             label
           >
             {data.map((_, index) => (
-              <Cell key={`cell-${index}`} fill={COLORS[index]} />
+              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
             ))}
+            <Label
+          value={`${percentage}%`}
+          position="center"
+          style={{ fontSize: "30px", fontWeight: "700" }}
+        />
           </Pie>
         </PieChart>
       </ResponsiveContainer>
