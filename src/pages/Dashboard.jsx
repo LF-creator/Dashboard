e7f52d1cfd152628d21251ab0c0e0b0a8224cc62
LF-creator <linar.fatkullin@yh.nackademin.se>
2025-06-10 15:33:21 +0200

    New icons dependecy

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -8,12 +8,15 @@ import SkeletonCard from '../components/SkeletonCard';
 import '../styles/Dashboard.css';
 
 import {
-  FaUser,
-  FaClock,
+  FaRegUser,
+  FaRegClock,
   FaEye,
   FaCar,
-  FaUsers,
+  FaUserFriends,
   FaChartPie,
+  FaRedo,
+  LuEye
+
 } from 'react-icons/fa';
 
 
@@ -44,12 +47,12 @@ if (!data) {
     {/* Metric Cards */}
     <h1 className="dashboard-title">Dashboard</h1>
     <section className="card-grid">
-      <Card label="Unique Contacts" value={data.uniqueContacts} icon={FaUser} />
-      <Card label="Frequency" value={data.frequency} icon={FaClock} />
-      <Card label="RAC" value={data.rac} icon={FaEye} />
-      <Card label="Aggregated Audience" value={data.aggregatedAudience} icon={FaUsers} />
-      <Card label="Avg. Observation Time" value={data.observationTimeAvg + 's'} icon={FaClock} />
-      <Card label="Total Observation Time" value={data.observationTimeTotal} icon={FaClock} />
+      <Card label="Unique Contacts" value={data.uniqueContacts} icon={FaRegUser} />
+      <Card label="Frequency" value={data.frequency} icon={FaRedo} />
+      <Card label="RAC" value={data.rac} icon={LuEye} />
+      <Card label="Aggregated Audience" value={data.aggregatedAudience} icon={FaUserFriends} />
+      <Card label="Avg. Observation Time" value={data.observationTimeAvg + 's'} icon={FaEye} />
+      <Card label="Total Observation Time" value={data.observationTimeTotal} icon={FaRegClock} />
       <Card label="Vehicles" value={data.vehicles} icon={FaCar} />
       <Card label="SOV" value={data.sov + '%'} icon={FaChartPie} />
     </section>
